5949187b923f109117dd176901e5fc3a7b406bd9
Enzo Martinez <35664442+enzo1000@users.noreply.github.com>
2022-02-23 17:03:32 +0100

    Moin de commentaire

--- ptut/connexion.php
+++ ptut/connexion.php
@@ -1,88 +1,23 @@
 <?php
 
 
-$user = 'root';
-$password = 'root';
-$db = 'ptut';
-$host = 'localhost';
-$port = 3307;
+    $user = 'root';
+    $password = 'root';
+    $db = 'ptut';
+    $host = 'localhost';
+    $port = 3307;
 
-try {
-      $conn = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $password);
+    try {
+          $conn = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $password);
 
-      $conn->setAttribute(PDO::ATTR_ERRMODE,
-          PDO::ERRMODE_EXCEPTION);
-
-      //echo 'Connexion réussie';
-
-} catch (PDOException $e) {
-      echo "Erreur : " . $e->getMessage();
-}
-
-
-/*
-
-define('HOST', 'localhost'); // déclaration en local ( à changer apres)
-define('DB_NAME', 'burillec');//nom de la bdd
-define('USER', 'burillec'); // nom de l'utilisateur (compte)
-define('PASS', '123456789'); // mot de passe de l'utilisateur (compte)
-
-try {
-    $conn = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME, USER, PASS,
-        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connection ok";
-} catch (PDOException $e) {
-    echo $e;
-}
-
-//https://webinfo.iutmontp.univ-montp2.fr/~burillec/testbdd/
-
-*/
-
-/*
-      $user = 'root';
-      $password = 'root';
-      $db = 'ptut';
-      $host = 'localhost';
-      $port = 3307;
-
-      try {
-      $conn = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $password,
           $conn->setAttribute(PDO::ATTR_ERRMODE,
-    PDO::ERRMODE_EXCEPTION
+              PDO::ERRMODE_EXCEPTION);
 
-     );
+          //echo 'Connexion réussie';
 
-*/
-      //echo 'Connexion réussie';
-/*
-<?php
+    } catch (PDOException $e) {
+          echo "Erreur : " . $e->getMessage();
+    }
 
-
-define('HOST', 'localhost'); // déclaration en local ( à changer apres)
-define('DB_NAME', 'burillec');//nom de la bdd
-define('USER', 'burillec'); // nom de l'utilisateur (compte)
-define('PASS', '123456789'); // mot de passe de l'utilisateur (compte)
-
-
-
-try {
-    $conn = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME, USER, PASS,
-        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connection ok";
-} catch (PDOException $e) {
-    echo $e;
-}
-
-//https://webinfo.iutmontp.univ-montp2.fr/~burillec/testbdd/
-
-
-      }
-      catch(PDOException $e){
-      echo "Erreur : " . $e->getMessage();
-      }
-*/
-   ?>
+?>
 
